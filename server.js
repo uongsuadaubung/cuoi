309c724bcf02589cccdf173fa5008ae27c81c762
kien.hm <kien.hm@clickcure.vn>
2024-12-11 11:32:30 +0700

    finish

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const fs = require('fs');
 const diaryPath = path.join(__dirname, 'diary.txt');
 const confirmationPath = path.join(__dirname, 'confirmation.txt');
 
-
+const xlsx = require('xlsx');
 
 
 const saveFile = (path, data) => {
@@ -19,10 +19,9 @@ const saveFile = (path, data) => {
 }
 
 const readFile = (path) => {
-    let data = "";
+    let data = "[]";
     if (!fs.existsSync(path)) {
-        saveFile(path,[]);
-        data = [];
+        saveFile(path,data);
     }else {
         data = fs.readFileSync(path, 'utf8');
     }
@@ -45,14 +44,30 @@ app.post('/api/confirmation', (req, res) => {
     //   "will_you_attend": "I will definitely attend",
     //   "whose_guest_are_you": "Bride"
     // }
-    if (!confirmationData.find(item=>{
-        item.your_name === receivedData.your_name &&
-            item.wedding_ceremony === receivedData.wedding_ceremony &&
-            item.groom_reception === receivedData.groom_reception &&
-            item.will_you_attend === receivedData.will_you_attend &&
-            item.whose_guest_are_you === receivedData.whose_guest_are_you
-    })){
-        confirmationData.unshift(receivedData);
+    if (!receivedData.your_name || !receivedData.wedding_ceremony || !receivedData.groom_reception || !receivedData.will_you_attend || !receivedData.whose_guest_are_you) {
+        res.status(400).json({ message: 'Không nhận được được dữ liệu!' });
+        return;
+    }
+    const newData = {
+        your_name: receivedData.your_name.trim(),
+        wedding_ceremony: receivedData.wedding_ceremony.trim(),
+        groom_reception: receivedData.groom_reception.trim(),
+        will_you_attend: receivedData.will_you_attend.trim(),
+        whose_guest_are_you: receivedData.whose_guest_are_you.trim()
+    }
+
+    let find = false;
+    confirmationData.forEach(item => {
+        if (item.your_name === newData.your_name
+            && item.wedding_ceremony === newData.wedding_ceremony
+            && item.groom_reception === newData.groom_reception
+            && item.will_you_attend === newData.will_you_attend
+            && item.whose_guest_are_you === newData.whose_guest_are_you){
+            find = true;
+        }
+    })
+    if (!find){
+        confirmationData.unshift(newData);
         saveFile(confirmationPath,confirmationData);
     }
 
@@ -66,8 +81,18 @@ app.get('/api/diary', (req, res) => {
 })
 app.post('/api/diary', (req, res) => {
     const receivedData = req.body;
-    if (!diaryData.find(item => item.name === receivedData.name && item.message === receivedData.message)) {
-        diaryData.unshift(receivedData);
+
+    if (!receivedData.name || !receivedData.message) {
+        res.status(400).json({ message: 'Không nhận được được dữ liệu!' });
+        return;
+    }
+    const newData = {
+        name: receivedData.name.trim(),
+        message: receivedData.message.trim(),
+        time: receivedData.time,
+    }
+    if (!diaryData.find(item => item.name === newData.name && item.message === newData.message)) {
+        diaryData.unshift(newData);
         saveFile(diaryPath,diaryData);
     }
 
@@ -75,16 +100,36 @@ app.post('/api/diary', (req, res) => {
 })
 
 app.get('/excel-diary', (req, res) => {
-
+    const workBook = xlsx.utils.book_new();
+    const workSheet = xlsx.utils.json_to_sheet(diaryData);
+    xlsx.utils.book_append_sheet(workBook, workSheet, 'Sheet1');
+    const filePath = path.join(__dirname, 'data.xlsx');
+    xlsx.writeFile(workBook, filePath);
+    res.download(filePath, "data.xlsx", (err) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error sending file');
+        }
+    });
 });
 
 app.get('/excel-confirmation', (req, res) => {
-
+    const workBook = xlsx.utils.book_new();
+    const workSheet = xlsx.utils.json_to_sheet(confirmationData);
+    xlsx.utils.book_append_sheet(workBook, workSheet, 'Sheet1');
+    const filePath = path.join(__dirname, 'confirm.xlsx');
+    xlsx.writeFile(workBook, filePath);
+    res.download(filePath, "confirm.xlsx", (err) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error sending file');
+        }
+    });
 })
 
 
 // Middleware để xử lý lỗi 404
-app.use((req, res, next) => {
+app.use((req, res) => {
     res.status(404).send('404 - Không tìm thấy trang!');
 });
 
